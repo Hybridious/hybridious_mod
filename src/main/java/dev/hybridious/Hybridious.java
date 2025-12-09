efe3385283a036b07252f5de0a48a28ef932ce9b
Hybridious <me@rhodiym.com>
2025-12-08 22:32:41 -0600

    Update Hybridious.java
    RocketSpeed

--- src/main/java/dev/hybridious/Hybridious.java
+++ src/main/java/dev/hybridious/Hybridious.java
@@ -26,7 +26,8 @@ public class Hybridious extends MeteorAddon {
         Modules.get().add(new SoundOnSneak());
         Modules.get().add(new MapFilterModule());
         Modules.get().add(new DropTest());
-        Modules.get().add(new TabGuiScale());       
+        Modules.get().add(new TabGuiScale()); 
+        Modules.get().add(new RocketSpeed()); 
         // Commands
         Commands.add(new MapFilterCommand());
         Commands.add(new BannerBlacklist());
